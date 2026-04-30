487485015a31aefb52d3fad6308c5e339bfa95b7
Danny-Dev400 <73005373+Danny-Dev400@users.noreply.github.com>
2026-04-30 12:29:38 -0500

    Update Playwright config to set retries and workers - for testing purposes for azure devops no pararllel policy

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,8 @@ export default defineConfig({
   testDir: "./src/tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 1 : 3,
+  retries: 0,
+  workers: 1,
   timeout: TEST_TIMEOUT,
   reporter: [
     ['list'],
